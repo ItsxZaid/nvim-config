cacd2435ff02b3163b24c5729a06a0d58f46fc0d
ItsxZaid <zaidzeenathe@gmail.com>
2025-11-17 13:29:40 +0530

    fix some keymaps in LSP-Completion

--- lua/plugins/lsp-completion.lua
+++ lua/plugins/lsp-completion.lua
@@ -10,7 +10,6 @@ return {
 		config = function()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 			cmp.setup({
 				sources = cmp.config.sources({
@@ -20,11 +19,45 @@ return {
 					{ name = "path" },
 				}),
 				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					-- Stop tabbing. Use C-n/C-p. It's faster.
+					-- Your hands stay on the home row.
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+
 					["jk"] = cmp.mapping.abort(),
 					["kj"] = cmp.mapping.abort(),
+
+					-- This is your new "Smart Enter"
+					-- If the menu is open, it confirms the highlighted item.
+					-- If the menu is closed, it's just a regular Enter.
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true, -- This is the key. It confirms the selected item.
+					}),
+
+					-- This is your new "Smart Tab"
+					-- It will do 3 things, in order:
+					-- 1. If menu is open, go to the next item.
+					-- 2. If on a snippet, jump to the next snippet placeholder.
+					-- 3. Otherwise, just be a normal Tab.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }), -- 'i' for insert mode, 's' for snippet mode
+
+					-- Smart Shift-Tab (goes backwards)
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				snippet = {
 					expand = function(args)
